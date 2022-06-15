6c4b35b6c101c25e884e167dda5811b1a5b56af3
Brain Philemon <philbrainy@gmail.com>
2022-06-15 14:27:15 +0100

    bug: Injectable decorator

--- src/database/repository/category.repository.ts
+++ src/database/repository/category.repository.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { Category } from 'src/entities/category.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(Category)
+@Injectable()
 export class CategoryRepository extends Repository<Category> {}
