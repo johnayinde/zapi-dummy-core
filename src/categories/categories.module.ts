2f2729e457e08bd87a02afce20b28b0c5738387d
Brain Philemon <philbrainy@gmail.com>
2022-06-29 09:10:11 +0100

    typeorm related changes to categories module

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Category } from 'src/entities/category.entity';
 import { CategoryRepository } from '../database/repository/category.repository';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CategoryRepository])],
+  imports: [TypeOrmModule.forFeature([Category])],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
