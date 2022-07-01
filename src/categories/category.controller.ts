2c589ec985556926286939c6eca7594c1df23e9b
Tope Olotu <topeolotu75@gmail.com>
2022-07-01 11:11:28 +0100

    chh

--- src/categories/category.controller.ts
+++ src/categories/category.controller.ts
@@ -7,7 +7,7 @@ import {
   ValidationPipe,
   Delete,
   Param,
-  ParseUUIDPipe
+  ParseUUIDPipe,
 } from '@nestjs/common';
 import { ZapiResponse } from '../common/helpers/response/Response';
 import { Ok } from 'src/common/helpers/response/ResponseType';
@@ -16,14 +16,13 @@ import { Category } from '../entities/category.entity';
 import { CreateCategoriesDto } from './dto/categories.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
-
 @ApiTags('Categories')
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryservice: CategoryService) {}
 
   @Post('create')
-  @ApiOperation({summary:'Creates a new category for an api'})
+  @ApiOperation({ summary: 'Creates a new category for an api' })
   @UsePipes(ValidationPipe)
   async createCategories(
     @Body()
@@ -34,21 +33,21 @@ export class CategoryController {
   }
 
   @Get()
-  @ApiOperation({summary:'fetches all categories present in the Database'})
+  @ApiOperation({ summary: 'fetches all categories present in the Database' })
   async getCategory(): Promise<Ok<Category[]>> {
     const allCategories = await this.categoryservice.getCategory();
-    return ZapiResponse.Ok(allCategories, 'Ok', '200')
+    return ZapiResponse.Ok(allCategories, 'Ok', '200');
   }
 
   @Delete(':id')
-  @ApiOperation({summary:'Deletes a single category with the matched id'})
+  @ApiOperation({ summary: 'Deletes a single category with the matched id' })
   async deleteCategogry(@Param('id', new ParseUUIDPipe()) id: string) {
     const category = await this.categoryservice.deleteCategogry(id);
     return ZapiResponse.Ok(category, 'Ok', '200');
   }
 
   @Get(':id')
-  @ApiOperation({summary:'Fetches a single category with the matched id'})
+  @ApiOperation({ summary: 'Fetches a single category with the matched id' })
   async findOneById(@Param('id', new ParseUUIDPipe()) id: string) {
     const category = await this.categoryservice.findOneById(id);
     return ZapiResponse.Ok(category, 'Ok', '200');
