f416386db25b2af3bfe416438eee08ef411a4a70
alahirajeffrey <alahirajeffrey@gmail.com>
2022-07-05 08:54:32 -1200

    added create tutorial dto

---
+++ src/tutorial/dto/create-tutorial-dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger/dist/decorators/api-property.decorator";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class TutorialDto {
+
+    @ApiProperty()
+    @IsNotEmpty()
+    title: string;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    body: string;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    apiId: string
+}
