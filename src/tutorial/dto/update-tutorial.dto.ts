0777c0f2efca84d9827fd764cf06c6743e9ea849
alahirajeffrey <alahirajeffrey@gmail.com>
2022-07-05 08:54:57 -1200

    added update tutorial dto

---
+++ src/tutorial/dto/update-tutorial.dto.ts
@@ -0,0 +1,13 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+
+export class UpdateTutorialDto {
+
+    @ApiPropertyOptional()   
+    title: string;
+
+    @ApiPropertyOptional()
+    body: string;
+
+    @ApiPropertyOptional()
+    apiId: string
+}
