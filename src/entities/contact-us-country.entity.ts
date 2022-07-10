201da662d6f49c1790e9a6d2ed79aab29e4b92e6
Adebiyi Oluwarotimi <66860398+danifara3@users.noreply.github.com>
2022-07-10 18:00:43 +0100

    deleted unused entities

--- src/entities/contact-us-country.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Column } from 'typeorm';
-import { SharedEntity } from '../common/model/sharedEntity';
-
-@Entity()
-export class ContactUsCountry extends SharedEntity {
-  @Column()
-  countryName: string;
-}
--- src/entities/contact-us-topic.entity.ts
+++
@@ -1,8 +0,0 @@
-import { Entity, Column } from 'typeorm';
-import { SharedEntity } from '../common/model/sharedEntity';
-
-@Entity()
-export class ContactUsTopic extends SharedEntity {
-  @Column()
-  topicName: string;
-}
